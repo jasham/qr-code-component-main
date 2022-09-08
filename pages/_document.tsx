50d1c87deda7698fd80d322b4d1bc970d56fbae5
Jasham <jasham009@gmail.com>
2022-09-09 04:46:59 +0530

    updating code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
-    <Html className="m-0 p-0 box-border">
+    <Html className="m-0 p-0 box-border" lang="en">
       <Head>
-        <meta charSet="UTF-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
